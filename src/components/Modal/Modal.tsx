2414059bcae64adfda8cbec03812f38178ad34f1
Ilya Tishin <nenaviju4tverg@gmail.com>
2023-07-09 19:37:49 -0700

    init commit #4

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,19 +3,27 @@ import { createPortal } from 'react-dom'
 
 type ModalProps = {
   children: ReactElement
-  onClose: VoidFunction
+  setIsOpened: (value: boolean) => void
+  isOpened: boolean
 }
 
-const Modal: FC<ModalProps> = ({ children, onClose }) => {
+const Modal: FC<ModalProps> = ({ children, setIsOpened, isOpened }) => {
+  const onClose = () => setIsOpened(false)
+
   const clickHandle = (e: MouseEvent) => {
     e.stopPropagation()
     onClose()
+  }
+
+  if (!isOpened) {
+    return null
   }
 
   return createPortal(
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions
     <div
       role="dialog"
+      tabIndex={-1}
       onClick={clickHandle}
       style={{
         position: 'fixed',
@@ -37,7 +45,6 @@ const Modal: FC<ModalProps> = ({ children, onClose }) => {
           borderRadius: 3,
           boxShadow: '0 0 24px rgba(0, 0, 0, 0.2)',
         }}
-        onKeyDown={(e) => e.stopPropagation()}
       >
         {children}
       </div>
@@ -46,41 +53,30 @@ const Modal: FC<ModalProps> = ({ children, onClose }) => {
   )
 }
 
-type ModalWrapperProps = {
-  children: ReactElement
-  onClose: VoidFunction
-}
-
-const ModalWrapper: FC<ModalWrapperProps> = ({ children, onClose }) => {
-  useEffect(() => {
-    const documentKeyboardHandler = (e: KeyboardEvent) => {
-      if (e.key === 'Escape') {
-        onClose()
-      }
-    }
-    document.addEventListener('keydown', documentKeyboardHandler)
-    return () => {
-      document.removeEventListener('keydown', documentKeyboardHandler)
-    }
-  }, [onClose])
-
-  return <Modal onClose={onClose}>{children}</Modal>
-}
-
 const ModalManager = () => {
   const [isOpened1, setIsOpened1] = useState(false)
   const [isOpened2, setIsOpened2] = useState(false)
   const [isOpened3, setIsOpened3] = useState(false)
 
-  const onClose1 = () => setIsOpened1(false)
-  const onClose2 = () => {
-    setIsOpened1(true)
-    setIsOpened2(false)
-  }
-  const onClose3 = () => {
-    setIsOpened2(true)
-    setIsOpened3(false)
-  }
+  useEffect(() => {
+    const documentKeyboardHandler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        if (isOpened3) {
+          setIsOpened3(false)
+        } else if (isOpened2) {
+          setIsOpened2(false)
+        } else if (isOpened1) {
+          setIsOpened1(false)
+        }
+      }
+    }
+
+    document.addEventListener('keydown', documentKeyboardHandler)
+
+    return () => {
+      document.removeEventListener('keydown', documentKeyboardHandler)
+    }
+  }, [isOpened1, isOpened2, isOpened3])
 
   return (
     <div className="App">
@@ -88,34 +84,40 @@ const ModalManager = () => {
         Открыть первое окно
       </button>
 
-      {isOpened1 && (
-        <ModalWrapper onClose={onClose1}>
-          <button type="button" onClick={() => setIsOpened2(true)}>
-            Открыть второе окно
-          </button>
-        </ModalWrapper>
-      )}
-      {isOpened2 && (
-        <ModalWrapper onClose={onClose2}>
-          <button type="button" onClick={() => setIsOpened3(true)}>
-            Открыть третье окно
-          </button>
-        </ModalWrapper>
-      )}
-      {isOpened3 && (
-        <ModalWrapper onClose={onClose3}>
-          <button
-            type="button"
-            onClick={() => {
-              setIsOpened1(false)
-              setIsOpened2(false)
-              setIsOpened3(false)
-            }}
-          >
-            Закрыть все окна
-          </button>
-        </ModalWrapper>
-      )}
+      <Modal setIsOpened={setIsOpened1} isOpened={isOpened1}>
+        <button
+          type="button"
+          onClick={(e) => {
+            setIsOpened2(true)
+            e.stopPropagation()
+          }}
+        >
+          Открыть второе окно
+        </button>
+      </Modal>
+      <Modal setIsOpened={setIsOpened2} isOpened={isOpened2}>
+        <button
+          type="button"
+          onClick={(e) => {
+            setIsOpened3(true)
+            e.stopPropagation()
+          }}
+        >
+          Открыть третье окно
+        </button>
+      </Modal>
+      <Modal setIsOpened={setIsOpened3} isOpened={isOpened3}>
+        <button
+          type="button"
+          onClick={() => {
+            setIsOpened1(false)
+            setIsOpened2(false)
+            setIsOpened3(false)
+          }}
+        >
+          Закрыть все окна
+        </button>
+      </Modal>
     </div>
   )
 }
